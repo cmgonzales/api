f17a0b38b8dec699b039defb1862aa4e1d4f01cf
Chris Gonzales <cm.gonzales@yahoo.com>
2018-08-10 20:25:24 -0700

    fixed input to api

--- scipt.js
+++ scipt.js
@@ -1,9 +1,3 @@
-var crimesapi = "";
-
-console.log(crimesapi)
-const crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
-
-
 
 
 document.getElementById('btn').addEventListener("click", function(){
@@ -12,6 +6,8 @@ document.getElementById('btn').addEventListener("click", function(){
 var crimes = document.getElementById('crimes').value;
 
 crimesapi = crimes.toUpperCase();
+
+const crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
 
 console.log(crimesapi)
 $.ajax({
