40deda8cc9ac215bddf2c842d38e52607defc9ce
Chris Gonzales <cm.gonzales@yahoo.com>
2018-08-11 17:41:32 -0700

    alert and enter functions

--- scipt.js
+++ scipt.js
@@ -1,15 +1,41 @@
 
+crimes.addEventListener("keyup", function(event){
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("btn").click();
+    }
+});
 
-document.getElementById('btn').addEventListener("click", function(){
+function datas(){
+
+    
+var crimes = document.getElementById('crimes').value;  
+
+     var crimesapi = crimes.toUpperCase(); 
+    
+var sfcrimes = ["FRAUD", "WARRANTS", "TRESPASS", "empty"]
 
 
-var crimes = document.getElementById('crimes').value;
+for(var i = 0; i < sfcrimes.length; i++){
+    
+    if(crimesapi === sfcrimes[i]) {
+        
+    console.log("test")
+    init()
 
-crimesapi = crimes.toUpperCase();
+        break;
 
-const crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
+     } if(sfcrimes[i] === "empty"){
+         
+         alert("Please enter crime categories");
+    
+     }
+}
 
-console.log(crimesapi)
+function init(){
+
+crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
+
 $.ajax({
     type: 'GET',
     url: crime,
@@ -24,11 +50,12 @@ $.ajax({
       content += ' Date: ' + data[i].date;
 
             $(".crime").append('<li>' + content + '</li>');
-
-      
     
         }
     }
 });
 
-})
+
+}
+
+}
