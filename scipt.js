ebcb8c62b68fe2f70551670b4a9c40fbc7923c07
Chris Gonzales <cm.gonzales@yahoo.com>
2018-08-11 19:01:11 -0700

    delete function init() smaller code

--- scipt.js
+++ scipt.js
@@ -1,4 +1,3 @@
-
 crimes.addEventListener("keyup", function(event){
     event.preventDefault();
     if (event.keyCode === 13) {
@@ -21,20 +20,7 @@ for(var i = 0; i < sfcrimes.length; i++){
     if(crimesapi === sfcrimes[i]) {
         
     console.log("test")
-    init()
-
-        break;
-
-     } if(sfcrimes[i] === "empty"){
-         
-         alert("Please enter crime categories");
-    
-     }
-}
-
-function init(){
-
-crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
+    crime = "https://data.sfgov.org/resource/cuks-n6tp.json?pddistrict=MISSION&dayofweek=Friday&$order=date%20DESC&$where=date%3E%272018-01-01T12:00:00%27&category=" + crimesapi
 
 $.ajax({
     type: 'GET',
@@ -54,8 +40,14 @@ $.ajax({
         }
     }
 });
+        break;
 
-
+     } if(sfcrimes[i] === "empty"){
+         
+         alert("Please enter crime categories");
+    
+     }
 }
+
 
 }
